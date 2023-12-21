26c2c06b90e684f6ddfde34522af2946d323d7f8
434huzaifa <saadhuzaifa2497@gmail.com>
2023-12-21 22:17:29 +0600

    jason web token aadded

--- index.js
+++ index.js
@@ -19,3 +19,48 @@ app.use(cors({
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@saaddb.bmj48ga.mongodb.net/SurveySphere?retryWrites=true&w=majority`
 mongoose.connect(uri)
+
+async function logger(req, res, next) {
+    let date = new Date()
+    console.log(date.toLocaleString("en-US"), ":", req.method, ":", req.url);
+    next()
+}
+
+const isThisToken = async (req, res, next) => {
+    const token = req?.cookies?.tasky;
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized" })
+    }
+    jwt.verify(token, process.env.TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ message: "Unauthorized" })
+        }
+        req.user = decoded
+        next()
+    })
+}
+
+async function run() {
+    try {
+        app.post('/jsonwebtoken', logger, async (req, res) => {
+            const user = req.body
+            const userid = await MyUser.findOne({ email: user.email })
+            user.userid = userid._id
+            const token = jwt.sign(user, process.env.TOKEN, { expiresIn: '1h' })
+            res.cookie('tasky', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none',
+            }).send({ success: true })
+        })
+        app.post('/logout', logger, isThisToken, async (req, res) => {
+            res.clearCookie('tasky', { maxAge: 0, sameSite: "none", secure: true, httpOnly: true }).send({ success: true })
+        })
+} catch (e) {
+        console.log(`The Error is:${e.message}`);
+        return
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => { res.send("Backend Running") });
+app.listen(port, () => { console.log(`Server Started at ${port}`) });
